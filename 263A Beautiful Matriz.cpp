532c4e1457ba4e63b1ce145fa7cab1b1f7810351
David Samuel Rios <davidsamuelrios07@gmail.com>
2019-01-31 13:17:35 -0400

    263A Beatutiful Matrix.cpp Accepted

--- 263A Beautiful Matriz.cpp
+++ 263A Beautiful Matriz.cpp
@@ -1,25 +1,21 @@
 #include <iostream>
-#include <math.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main(){
-  int fila, columna;
-  bool matriz[5][5];
-  for(int i=1; i<=5; i++){
-    for(int j=1; j<=5; j++){
-      cin >> matriz[i][j];
-      if(matriz[i][j] == 1){
-        fila = i;
-        columna = j;
-      }
+int main()
+{
+    int arr[5][5],i , j ;
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<5;j++)
+        {
+            cin>>arr[i][j];
+            if(arr[i][j]!=0)
+            {
+                cout<<abs(i-2)+abs(j-2);
+                return 0 ;
+            }
+        }
     }
-  }
-  int val_filas, val_columnas;
-  val_filas = abs(3-fila); val_columnas = abs(3-columna);
-  int pasos = val_filas + val_columnas;
-  cout << pasos;
-
-
-  return 0;
 }
